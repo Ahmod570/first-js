2b30462108a7a2863f0b9cf91a6284c3f594b2e4
Ahmod570 <mustakimahmodsany@gmail.com>
2025-10-14 11:24:32 +0600

    first js

---
+++ index.js
@@ -0,0 +1,61 @@
+
+var number = 5000 ; 
+var ita_string = "string corner";
+var okonLekmuBoolean = false ;
+var ItaHoiloGiaNull = null ;
+var alu_piazz_moris_sobji = 20+40+500+200  ; 
+var dukanDarReDisi = 1000+1 ;
+var KaliAluKinleDukanderDibe = dukanDarReDisi-DoKgAluHoilo ;
+var alu_poti_kg = 20 ;
+var DoKgAluHoilo = 20*10 ;
+var kali_alur_dam =  dukanDarReDisi -  DoKgAluHoilo ;
+var AkHaxzarTkDiaAluAnaJaib0 = dukanDarReDisi / alu_poti_kg  ;
+// console. log(AkHaxzarTkDiaAluAnaJaib0);
+
+var komla =300 ;
+var AmarSunarBangla = "ami tumai balobashi" ;
+var apni_js_paroin_ni = false;
+var itaHoiloJanina = undefined; 
+var LastOneHoiloNull = null ;
+
+
+
+var piaz = 20;
+var alu = 20;
+var  masala = 100;
+var MutHoise = piaz+alu+masala;
+var amar_daro_ase = 500;
+var firotPaimu = amar_daro_ase- MutHoise;
+var ChairKgPiaz =  piaz*4 +1; 
+ var bag = ChairKgPiaz/2  ;
+ var thole = ChairKgPiaz % 2;
+ var gun = 2*8 ;
+ var BoroGun = 0.2*0.5;
+ var komlaArokDukano ="400.02";
+ var NumberKortam = parseInt(komlaArokDukano);
+var number_kortam = parseFloat(komlaArokDukano);
+var EkTakLavKorse = ++piaz ;
+var ek_taka_loss_hoise = --alu;
+var k_boro = piaz>alu;
+var ChotoK = masala<alu;
+var HeBoroNaSoman = piaz>=masala;
+alu +=20;
+piaz -=1;
+masala *=2;
+masala /=2;
+const jani = 80 ;
+
+
+
+
+// array
+var egg_case = [30, null,"komla", false, undefined, 7000 ] ;
+var kissu_deka = egg_case[3] ;
+var undefined_bar_kortam =egg_case .indexOf(undefined) ;
+egg_case .unshift("First Product") ;
+egg_case.shift() ;
+
+
+egg_case.push("last one") ;
+egg_case.pop()
+console. log(egg_case );
